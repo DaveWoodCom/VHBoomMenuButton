8fc49e04605490a8950b814ba52cff21822ba13d
Dave Wood <dave@cerebralgardens.com>
2020-01-23 12:03:15 -0500

    Change shadowPathRect to a non-optional, set a default value (fixes @IBInspectable)

--- VHBoomMenuButtonSwift/BoomMenuButton/BoomMenuButton.swift
+++ VHBoomMenuButtonSwift/BoomMenuButton/BoomMenuButton.swift
@@ -55,7 +55,7 @@ public class BoomMenuButton: UIView, BoomButtonDelegate, BackgroundDelegate {
     /// Rect (in points) of shadow path of BMB.
     ///
     /// The default value is (2, 2, BMB.width - 4, BMB.height - 4).
-    @IBInspectable public var shadowPathRect: CGRect! {
+    @IBInspectable public var shadowPathRect: CGRect = CGRect.zero {
         didSet {
             if shadowPathRect != oldValue {
                 setNeedsDisplay()
